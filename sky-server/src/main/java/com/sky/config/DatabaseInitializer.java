dd1065363b6c8c274923ceb6ef4ea1fa83a13e47
yicLionel <lionelyyc@gmail.com>
2026-05-27 21:15:25 +1000

    Auto initialize Railway database

---
+++ sky-server/src/main/java/com/sky/config/DatabaseInitializer.java
@@ -0,0 +1,44 @@
+package com.sky.config;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+
+@Component
+public class DatabaseInitializer implements ApplicationRunner {
+    private final DataSource dataSource;
+    private final JdbcTemplate jdbcTemplate;
+
+    public DatabaseInitializer(DataSource dataSource, JdbcTemplate jdbcTemplate) {
+        this.dataSource = dataSource;
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public void run(ApplicationArguments args) {
+        if (hasTables()) {
+            return;
+        }
+
+        ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+        populator.setSqlScriptEncoding("UTF-8");
+        populator.addScript(new ClassPathResource("schema.sql"));
+        populator.addScript(new ClassPathResource("data.sql"));
+        populator.execute(dataSource);
+    }
+
+    private boolean hasTables() {
+        Integer count = jdbcTemplate.queryForObject("""
+                select count(*)
+                from information_schema.tables
+                where table_schema = database()
+                  and table_name = 'category'
+                """, Integer.class);
+        return count != null && count > 0;
+    }
+}
